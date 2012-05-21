68d04a41d2c43e8c691dc2535dc6e876c18cf1bd
saitoha <user@zuse.jp>
2012-05-21 23:52:11 +0900

    Treat Category Cf (Other,format) as non spacing.

--- modules/EastAsianWidthDetectorGenerator.pl
+++ modules/EastAsianWidthDetectorGenerator.pl
@@ -32,8 +32,8 @@ close(IN);
 
 open(IN, "UnicodeData.txt") or die;
 while (<IN>) {
-    #if ($_ =~ /^([0-9A-F]{4});[^;]*;(Mn|Mc|Me|Sk)/) {
-    if ($_ =~ /^([0-9A-F]{4});[^;]*;(Mn|Mc)/) {
+    # Mark Nonspacing / Mark, Enclosing / Other, format
+    if ($_ =~ /^([0-9A-F]{4});[^;]*;(Mn|Me|Cf)/) { 
         $wcmap1{$1} = 0;
         $wcmap2{$1} = 0;
     }
