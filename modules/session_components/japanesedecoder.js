14810562aadfe9f78779c6f341fa03307dcbd27d
saitoha <user@zuse.jp>
2012-09-01 15:59:16 +0900

    fix typo

---
+++ modules/session_components/japanesedecoder.js
@@ -0,0 +1,252 @@
+/* -*- Mode: JAVASCRIPT; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is tanasinn
+ *
+ * The Initial Developer of the Original Code is
+ * Hayaki Saito.
+ * Portions created by the Initial Developer are Copyright (C) 2010 - 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+/**
+ * @class JapaneseDecoder
+ */
+var JapaneseDecoder = new Class().extends(Component);
+JapaneseDecoder.definition = {
+
+  get id()
+    "japanese_decoder",
+
+  get scheme()
+    "japanese-js",
+
+  get info()
+    <module>
+        <name>{_("Japanese Decoder")}</name>
+        <version>0.1</version>
+        <description>{
+          _("Parallel decoder module for Japanese character sets.")
+        }</description>
+    </module>,
+
+  _cp932_map: null,
+  _jis0208_map: null,
+  _offset: 0,
+
+  /** Constructor **/
+  "[subscribe('get/decoders'), enabled]":
+  function getDecoders(map) 
+  {
+    return {
+      charset: this.scheme,
+      converter: this,
+      title: "Japanese parallel decoder implemented in js",
+    };
+  },
+
+  activate: function activate() 
+  {
+    var resource_path_cp932 = "modules/mappings/cp932.txt.js",
+        json_content_cp932 = coUtils.IO.readFromFile(resource_path_cp932),
+        mapping_cp932 = eval(json_content_cp932),
+        resource_path_jis0208 = "modules/mappings/jis0208.txt.js",
+        json_content_jis0208 = coUtils.IO.readFromFile(resource_path_jis0208),
+        mapping_jis0208 = eval(json_content_jis0208);
+
+    this._cp932_map = mapping_cp932.map;    
+    this._jis0208_map = mapping_jis0208.map;    
+  },
+
+  /** Parse UTF-8 string sequence and convert it 
+   *  to UCS-4 code point sequence. 
+   */
+  decode: function decode(scanner) 
+  {
+    var self = this;
+
+    return function(scanner)
+    {
+      var c;
+
+      while (!scanner.isEnd) {
+        c = self._getNextCharacter(scanner);// + offset;
+        if (c < 0x20 || 0x7f === c) {
+          break;
+        }
+        yield c;
+        scanner.moveNext();
+      };
+    } (scanner);
+  },
+
+  /** Decode UTF-8 encoded byte sequences 
+   *  and Return UCS-4 character set code point. 
+   *
+   *  @param {Scanner} scanner A scanner object that attached to 
+   *                   current input stream.
+   *  @return {Array} Converted sequence 
+   */
+  _getNextCharacter: function _getNextCharacter(scanner) 
+  {
+    var c = scanner.current(),
+        c2,
+        c3,
+        c4,
+        first,
+        second,
+        third,
+        fourth,
+        result;
+
+    if (c < 0x20 || 0x7f === c) {
+      return null;
+    } else if (c < 0x7f) { // 8bit (ASCII/DEC/ISO)
+      return c;
+    } else if (c < 0xe0) {
+      scanner.moveNext();
+      c2 = scanner.current();
+
+      // UTF-8
+      // 110xxxxx 10xxxxxx 
+      // (0x00000080 - 0x000007ff) // 11bit
+      if (0x6 === c >>> 5 && 0x2 === c2 >>> 6) {
+        first = (c & 0x1f) << 6;
+        second = c2 & 0x3f;
+        return first | second;
+      }
+
+      // CP-932
+      if (0x81 <= c && c <= 0x9f) { // cp932 first character
+        if ((0x40 <= c2 && c2 <= 0x7e) || (0x80 <= c2 && c2 <= 0xfc)) { // cp932 second character
+          code = c << 8 | c2;
+          result = this._cp932_map[code];
+          if (result) {
+            return result;
+          }
+        }
+      } 
+
+      result = this._jis0208_map[c - 0x80 << 8 | c2 - 0x80];
+      if (result) {
+        return result;
+      }
+
+    } else if (c < 0xf0) {
+      scanner.moveNext();
+      c2 = scanner.current();
+
+      // UTF-8
+      // 1110xxxx 10xxxxxx 10xxxxxx 
+      // (0x00000800 - 0x0000ffff) // 16bit (UCS-2)
+      if (0xe === c >>> 4 && 0x2 === c2 >>> 6) {
+        first = (c & 0xf) << 12;
+        second = (c2 & 0x3f) << 6;
+        scanner.moveNext();
+        c2 = scanner.current();
+
+        if (0x2 === c2 >>> 6) {
+          third = c2 & 0x3f;
+          return first | second | third;
+        }
+      }
+
+      // CP-932
+      if (0xe0 <= c && c <= 0xfc) { // cp932 first character
+        if ((0x40 <= c2 && c2 <= 0x7e) || (0x80 <= c2 && c2 <= 0xfc)) { // cp932 second character
+          code = (c << 8) | (c2);
+          result = this._cp932_map[code];
+          if (result) {
+            return result;
+          }
+        }
+      }
+
+      // EUF-JP
+      result = this._jis0208_map[c - 0x80 << 8 | c2 - 0x80];
+      if (result) {
+        return result;
+      }
+
+    } else if (c < 0xf8) {
+      scanner.moveNext();
+      c2 = scanner.current();
+
+      // UTF-8
+      // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 
+      // (0x00010000 - 0x001fffff) // 21bit (UCS-4)
+      if (0x1e === c >>> 3 && 0x2 === c >>> 6) {
+        first = (c & 0x7) << 18;
+        second = (c2 & 0x3f) << 12;
+
+        scanner.setAnchor();
+
+        scanner.moveNext();
+        c3 = scanner.current();
+        if (0x2 === c3 >>> 6) {
+          third = (c3 & 0x3f) << 6;
+          scanner.moveNext();
+          c4 = scanner.current();
+          if (0x2 === c4 >>> 6) {
+            fourth = c4 & 0x3f;
+            return first | second | third | fourth;
+          }
+        }
+
+        scanner.rollback();
+      }
+
+      // EUC-JP
+      result = this._jis0208_map[c - 0x80 << 8 | c2 - 0x80];
+      if (result) {
+        return result;
+      }
+
+    } else {
+      scanner.moveNext();
+      c2 = scanner.current();
+
+      if (c <= 0xfc) { // cp932 first character
+        if ((0x40 <= c2 && c2 <= 0x7e) || (0x80 <= c2 && c2 <= 0xfc)) { // cp932 second character
+          code = (c << 8) | (c2);
+          result = this._cp932_map[code];
+          if (result) {
+            return result;
+          }
+        }
+      }
+
+      result = this._jis0208_map[c - 0x80 << 8 | c2 - 0x80];
+      if (result) {
+        return result;
+      }
+
+      return null;
+    }
+  },
+}
+
+/**
+ * @fn main
+ * @brief Module entry point.
+ * @param {Broker} broker The Broker object.
+ */
+function main(broker) 
+{
+  new JapaneseDecoder(broker);
+}
+
+// EOF
---
+++ modules/session_components/utf8_cp932decoder.js
@@ -0,0 +1,179 @@
+/* -*- Mode: JAVASCRIPT; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is tanasinn
+ *
+ * The Initial Developer of the Original Code is
+ * Hayaki Saito.
+ * Portions created by the Initial Developer are Copyright (C) 2010 - 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+
+/**
+ * @class UTF8_CP932Decoder
+ */
+var UTF8_CP932Decoder = new Class().extends(Component);
+UTF8_CP932Decoder.definition = {
+
+  get id()
+    "utf8_cp932_decoder",
+
+  get scheme()
+    "utf8-cp932-js",
+
+  get info()
+    <module>
+        <name>{_("UTF-8 Decoder")}</name>
+        <version>0.1</version>
+        <description>{
+          _("Parallel decoder module for UTF-8/CP 932 character sets.")
+        }</description>
+    </module>,
+
+  _cp932_map: null,
+  _offset: 0,
+
+  /** Constructor **/
+  "[subscribe('get/decoders'), enabled]":
+  function getDecoders(map) 
+  {
+    return {
+      charset: this.scheme,
+      converter: this,
+      title: "UTF-8/Shift-JIS parallel decoder implemented in js",
+    };
+  },
+
+  activate: function activate() 
+  {
+    var resource_path_cp932 = "modules/mappings/cp932.txt.js",
+        json_content_cp932 = coUtils.IO.readFromFile(resource_path_cp932),
+        mapping_cp932 = eval(json_content_cp932);
+
+    this._cp932_map = mapping_cp932.map;    
+  },
+
+  /** Parse UTF-8 string sequence and convert it 
+   *  to UCS-4 code point sequence. 
+   */
+  decode: function decode(scanner) 
+  {
+    var self = this;
+
+    return function(scanner)
+    {
+      var c;
+
+      while (!scanner.isEnd) {
+        c = self._getNextCharacter(scanner);// + offset;
+        if (c < 0x20 || 0x7f === c) {
+          break;
+        }
+        yield c;
+        scanner.moveNext();
+      };
+    } (scanner);
+  },
+
+  /** Decode UTF-8 encoded byte sequences 
+   *  and Return UCS-4 character set code point. 
+   *
+   *  @param {Scanner} scanner A scanner object that attached to 
+   *                   current input stream.
+   *  @return {Array} Converted sequence 
+   */
+  _getNextCharacter: function _getNextCharacter(scanner) 
+  {
+    var c = scanner.current(),
+        c2,
+        first,
+        second,
+        third,
+        fourth;
+
+    if (c < 0x20 || 0x7f === c) {
+      return null;
+    } else if (c < 0x7f) { // 8bit (ASCII/DEC/ISO)
+      return c;
+    } else if (c < 0xe0) {
+      scanner.moveNext();
+      c2 = scanner.current();
+      if (0x81 <= c && c <= 0x9f) { // cp932 first character
+        if ((0x40 <= c2 && c2 <= 0x7e) || (0x80 <= c2 && c2 <= 0xfc)) { // cp932 second character
+          code = (c << 8) | (c2);
+          return this._cp932_map[code];
+        }
+        if (c < 0xa0) {
+          return null;
+        }
+      } 
+      // 110xxxxx 10xxxxxx 
+      // (0x00000080 - 0x000007ff) // 11bit
+      first = (c & 0x1f) << 6;
+      second = c2 & 0x3f;
+      return first | second;
+    } else if (c < 0xf0) {
+      scanner.moveNext();
+      c2 = scanner.current();
+      // 1110xxxx 10xxxxxx 10xxxxxx 
+      // (0x00000800 - 0x0000ffff) // 16bit (UCS-2)
+      if (0xe === c >>> 4) {
+        first = (c & 0xf) << 12;
+        if (0x2 === c2 >>> 6) {
+          second = (c2 & 0x3f) << 6;
+          scanner.moveNext();
+          c2 = scanner.current();
+          if (0x2 === c2 >>> 6) {
+            third = c2 & 0x3f;
+            return first | second | third;
+          }
+        }
+      }
+      if (0xe0 <= c && c <= 0xfc) { // cp932 first character
+        if ((0x40 <= c2 && c2 <= 0x7e) || (0x80 <= c2 && c2 <= 0xfc)) { // cp932 second character
+          code = (c << 8) | (c2);
+          return this._cp932_map[code];
+        }
+      }
+    } else if (c < 0xf8) {
+      // 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx 
+      // (0x00010000 - 0x001fffff) // 21bit (UCS-4)
+      first = (c & 0x7) << 18;
+      scanner.moveNext();
+      second = (scanner.current() & 0x3f) << 12;
+      scanner.moveNext();
+      third = (scanner.current() & 0x3f) << 6;
+      scanner.moveNext();
+      fourth = scanner.current() & 0x3f;
+      return first | second | third | fourth;
+    } else {
+      return null;
+    }
+  },
+}
+
+/**
+ * @fn main
+ * @brief Module entry point.
+ * @param {Broker} broker The Broker object.
+ */
+function main(broker) 
+{
+  new UTF8_CP932Decoder(broker);
+}
+
+// EOF
