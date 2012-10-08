e6c701ab9a91aea4f310cca048a79ab266ad1cac
saitoha <user@zuse.jp>
2012-10-08 17:32:10 +0900

    Disable mascot plugin by default

--- modules/session_components/mascot.js
+++ modules/session_components/mascot.js
@@ -65,7 +65,7 @@ Mascot.definition = {
     };
   },
 
-  "[persistable] enabled_when_startup": true,
+  "[persistable] enabled_when_startup": false,
 
   "[persistable] mascot_image_file": "images/mascot.svg",
   "[persistable] offset_left": -80,
