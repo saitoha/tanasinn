14a1517c1100a4954743d2a1defdd3748d3e0330
saitoha <user@zuse.jp>
2012-11-12 20:15:25 +0900

    Set origin when DECSTBM is called

--- modules/session_components/scrollregion.js
+++ modules/session_components/scrollregion.js
@@ -133,6 +133,7 @@ ScrollRegion.definition = {
     // DECSTBM moves the cursor to column 1, line 1 of the page.
     screen.setPositionX(0);
     screen.setPositionY(top);
+    cursor_state.setOrigin();
   },
 
   "[subscribe('sequence/decrqss/decstbm'), pnp]":
