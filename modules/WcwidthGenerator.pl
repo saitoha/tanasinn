0174354b37d5d8cbc27c19a2e925464b11b9d746
saitoha <user@zuse.jp>
2012-11-29 00:16:20 +0900

    Supports Hangul Jamo

--- modules/WcwidthGenerator.pl
+++ modules/WcwidthGenerator.pl
@@ -3,6 +3,8 @@
 use strict;
 my %wcmap1;
 my %wcmap2;
+my %wclmap1;
+my %wclmap2;
 
 open(IN, "EastAsianWidth.txt") or die;
 
@@ -26,6 +28,36 @@ while (<IN>) {
             $wcmap2{sprintf("%04x", $i)} = 2;
         }
     }
+
+#    if ($_ =~ /^([0-9A-F]{5});(F|W)/) {
+#        my $n = hex $1;
+#        my $first = sprintf "%04x", (($n >> 10) + 0xD800);
+#        my $second = sprintf "%04x", (($n & 0x3ff) + 0xDC00);
+#        $wclmap1{$first+""}{$second+""} = 2;
+#    } elsif ($_ =~ /^([0-9A-F]{5})\.\.([0-9A-F]{5});(F|W)/) {
+#        my $begin = hex $1;
+#        my $end = hex $2;
+#        for (my $i = $begin; $i <= $end; $i++) {
+#            my $first = sprintf "%04x", (($i >> 10) + 0xD800);
+#            my $second = sprintf "%04X", (($i & 0x3ff) + 0xDC00);
+#            $wclmap1{$first+""}{$second+""} = 2;
+#        }
+#    }
+#    if ($_ =~ /^([0-9A-F]{5});(F|W|A)/) {
+#        my $n = hex $1;
+#        my $first = sprintf "%04x", (($n >> 10) + 0xD800);
+#        my $second = sprintf "%04x", (($n & 0x3ff) + 0xDC00);
+#        $wclmap2{$first+""}{$second+""} = 2;
+#    } elsif ($_ =~ /^([0-9A-F]{5})\.\.([0-9A-F]{5});(F|W|A)/) {
+#        my $begin = hex $1;
+#        my $last = hex $2;
+#        for (my $i = $begin; $i <= $last; $i++) {
+#            my $first = sprintf "%04x", (($i >> 10) + 0xD800);
+#            my $second = sprintf "%04x", (($i & 0x3ff) + 0xDC00);
+#            $wclmap2{$first+""}{$second+""} = 2;
+#        }
+#    }
+
 }
 
 close(IN);
@@ -33,10 +65,23 @@ close(IN);
 open(IN, "UnicodeData.txt") or die;
 while (<IN>) {
     # Mark Nonspacing / Mark, Enclosing / Other, format
-    if ($_ =~ /^([0-9A-F]{4});[^;]*;(Mn|Me|Cf)/) { 
+    if ($_ =~ /^00AD;/) {
+        next;
+    }
+    if ($_ =~ /^([0-9A-F]{4});[^;]*;(Mn|Me|Cf)/
+     || $_ =~ /^11[6-9A-F][0-9A-F];/
+     || $_ =~ /^200B;/ )
+    { 
         $wcmap1{$1} = 0;
         $wcmap2{$1} = 0;
     }
+#    if ($_ =~ /^([0-9A-F]{5});[^;]*;(Mn|Me|Cf)/) { 
+#        my $n = hex $1;
+#        my $first = sprintf "%04x", (($n >> 10) + 0xD800);
+#        my $second = sprintf "%04x", (($n & 0x3ff) + 0xDC00);
+#        $wclmap1{$first}{$second} = 0;
+#        $wclmap2{$first}{$second} = 0;
+#    }
 }
 close(IN);
 
@@ -46,28 +91,39 @@ sub print_characters {
     my $previous = 0;
     my @results = ();
     foreach (sort keys %wcmap) {
-        if ($wcmap{$_} == $s) {
+        if (ref($wcmap{$_}) ne "HASH") {
             my $current = hex $_;
-            if ($previous == $current - 1) {
-                # pass
-            } else {
-                if ($last == $previous) {
-                    push @results, sprintf("\\u%04x", $current);
-                    $last = $current;
-                } elsif ($previous - $last <= 1) {
-                    push @results, sprintf("\\u%04x\\u%04x", $previous, $current);
-                    $last = $current;
+            if ($wcmap{$_} == $s) {
+                if ($previous == $current - 1) {
+                    # pass
                 } else {
-                    push @results, sprintf("-\\u%04x\\u%04x", $previous, $current);
-                    $last = $current;
+                    if ($last == $previous) {
+                        push @results, sprintf("\\u%04x", $current);
+                        $last = $current;
+                    } elsif ($previous - $last <= 1) {
+                        push @results, sprintf("\\u%04x\\u%04x", $previous, $current);
+                        $last = $current;
+                    } else {
+                        push @results, sprintf("-\\u%04x\\u%04x", $previous, $current);
+                        $last = $current;
+                    }
+                }
+                $previous = $current;
+            }
+        } else {
+            my $first = $_;
+            my $seconds = &print_characters($s, %{$wcmap{$first}});
+            if (length($seconds) > 0) {
+                if (length($seconds) < 8) {
+                    push(@results, sprintf "|\\u%s%s", $first, $seconds);
+                } else {
+                    push(@results, sprintf "|\\u%s[%s]", $first, $seconds);
                 }
             }
-            $previous = $current;
         }
     }
     return join "", @results;
 }
-
 print <<EOF;
 /* -*- Mode: JAVASCRIPT; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 4 -*-
  *
@@ -94,60 +150,23 @@ print <<EOF;
 
 "use strict";
 
-
 function wcwidth_amb_as_single(c) 
 {
     if (c < 0x10000) {
         var s = String.fromCharCode(c);
-        if (/[@{[print_characters 2, %wcmap1]}]/.test(s)) {
+        if (/^(?:[@{[print_characters 2, %wcmap1]}]|@{[print_characters 2, %wclmap1]})\$/.test(s)) {
             return 2;
-        } else if (/[@{[print_characters 0, %wcmap1]}]/.test(s)) {
+        } else if (/^(?:[@{[print_characters 0, %wcmap1]}]|@{[print_characters 0, %wclmap1]})\$/.test(s)) {
             return 0;
         }
-    } else if (c < 0x1F100) {
+    } else if (c < 0x1F200) {
         return 1;
     } else if (c < 0x1F300) {
-        switch (c) {
-            case 0x1F12E:
-            case 0x1F16A:
-            case 0x1F16B:
-            case 0x1F1E6:
-            case 0x1F1E7:
-            case 0x1F1E8:
-            case 0x1F1E9:
-            case 0x1F1EA:
-            case 0x1F1EB:
-            case 0x1F1EC:
-            case 0x1F1ED:
-            case 0x1F1EE:
-            case 0x1F1EF:
-            case 0x1F1F0:
-            case 0x1F1F1:
-            case 0x1F1F2:
-            case 0x1F1F3:
-            case 0x1F1F4:
-            case 0x1F1F5:
-            case 0x1F1F6:
-            case 0x1F1F7:
-            case 0x1F1F8:
-            case 0x1F1F9:
-            case 0x1F1FA:
-            case 0x1F1FB:
-            case 0x1F1FC:
-            case 0x1F1FD:
-            case 0x1F1FE:
-            case 0x1F1FF:
-                return 1;
-        }
         return 2;
     } else if (c < 0x20000) {
         return 1;
     } else if (c < 0xE0000) {
         return 2;
-    } else if (c < 0xE1000) {
-        return 1;
-    } else if (c > 0xFFFFF) {
-        return 1;
     }
     return 1;
 } 
@@ -156,23 +175,28 @@ function wcwidth_amb_as_double(c)
 {
     if (c < 0x10000) {
         var s = String.fromCharCode(c);
-        if (/[@{[print_characters 2, %wcmap2]}]/.test(s)) {
+        if (/^(?:[@{[print_characters 2, %wcmap2]}]|@{[print_characters 2, %wclmap2]})\$/.test(s)) {
             return 2;
-        } else if (/[@{[print_characters 0, %wcmap2]}]/.test(s)) {
+        } else if (/^(?:[@{[print_characters 0, %wcmap2]}]|@{[print_characters 0, %wclmap2]})\$/.test(s)) {
             return 0;
         }
     } else if (c < 0x1F100) {
         return 1;
-    } else if (c < 0x1F200) {
+    } else if (c < 0x1F1A0) {
+        if (c == 0x1F12E) {
+            return 1;
+        } else if (c == 0x1F16A) {
+            return 1;
+        } else if (c == 0x1F16B) {
+            return 1;
+        }
         return 2;
+    } else if (c < 0x1F200) {
+        return 1;
     } else if (c < 0x1F300) {
         return 2;
     } else if (c < 0x20000) {
         return 1;
-    } else if (c < 0xE0000) {
-        return 2;
-    } else if (c > 0xFFFFF) {
-        return 2;
     }
     return 2;
 }
