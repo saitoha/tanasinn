2d93335dce1c223000aa524775aa2902fc49237e
Hayaki Saito <user@zuse.jp>
2014-02-07 21:26:14 +0900

    Minor fix

--- modules/session_components/input.js
+++ modules/session_components/input.js
@@ -1195,6 +1195,8 @@ InputManager.definition = {
 
     event.preventDefault();
     event.stopPropagation();
+    
+    this.sendMessage("command/focus");
   },
 
   /** Keypress event handler.
@@ -1239,8 +1241,6 @@ InputManager.definition = {
         mode: coUtils.Constant.INPUT_MODE_NORMAL,
         event: event,
       });
-    
-    this.sendMessage("command/focus");
 
   },
 
